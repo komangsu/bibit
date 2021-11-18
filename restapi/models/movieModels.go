12c0aa39d2129395447bd3e91ea5dd439cb6d610
komangsu <mangokky@gmail.com>
2021-11-18 18:21:36 +0800

    update model detail movie

--- restapi/models/movieModels.go
+++ restapi/models/movieModels.go
@@ -1,23 +1,38 @@
 package models
 
 type DetailMovieResponse struct {
-	Title    string `json:"Title"`
-	Poster   string `json:"Poster"`
-	Type     string `json:"Type"`
-	Movie    string `json:"Movie"`
-	Year     string `json:"Year"`
-	ImdbID   string `json:"imdbID"`
-	Plot     string `json:"Plot"`
-	Rated    string `json:"Rated"`
-	Runtime  string `json:"Runtime"`
-	Genre    string `json:"Genre"`
-	Director string `json:"Director"`
-	Language string `json:"Language"`
-	Released string `json:"Released"`
-	Actors   string `json:"Actors"`
-	Writer   string `json:"Writer"`
-	Country  string `json:"Country"`
-	Awards   string `json:"Awards"`
+	Title      string `json:"Title"`
+	Poster     string `json:"Poster"`
+	Type       string `json:"Type"`
+	Movie      string `json:"Movie"`
+	Year       string `json:"Year"`
+	ImdbID     string `json:"imdbID"`
+	Plot       string `json:"Plot"`
+	Rated      string `json:"Rated"`
+	Runtime    string `json:"Runtime"`
+	Genre      string `json:"Genre"`
+	Director   string `json:"Director"`
+	Language   string `json:"Language"`
+	Released   string `json:"Released"`
+	Actors     string `json:"Actors"`
+	Writer     string `json:"Writer"`
+	Country    string `json:"Country"`
+	Awards     string `json:"Awards"`
+	Metascore  string `json:"Metascore"`
+	ImdbRating string `json:"imdbRating"`
+	ImdbVotes  string `json:"imdbVotes"`
+	DVD        string `json:"DVD"`
+	BoxOffice  string `json:"BoxOffice"`
+	Production string `json:"Production"`
+	Website    string `json:"Website"`
+	Response   string `json:"Response"`
+	Ratings    []Ratings
+}
+
+type Ratings struct {
+	Source   string `json:"Source"`
+	Database string `json:"Database"`
+	Value    string `json:"Value"`
 }
 
 type SearchMovieResponse struct {
